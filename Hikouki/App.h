ebcced639abfb696416b1d71b3a1d4056e14941b
lm9 <depkey@me.com>
2018-06-09 00:35:56 +0900

    mainをスッキリさせたかった

---
+++ Hikouki/App.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#pragma comment(lib, "d3d9.lib")			// リンク対象ライブラリにd3d9.libを追加
+#pragma comment(lib, "d3dx9.lib")			// リンク対象ライブラリにd3dx9.libを追加
+#pragma comment(lib, "winmm.lib")			// リンク対象ライブラリにwinmm.libを追加
+
+#include <windows.h>
+#include "GameController.h"
+
+class App
+{
+private:
+	const char* title = "Title";
+	const char* name = "name";
+	static constexpr unsigned width = 1280;
+	static constexpr unsigned height = 720;
+	static constexpr bool isFullscreen = false;
+	HWND wh;
+	HINSTANCE ins;
+	static LRESULT CALLBACK wndProc(HWND, UINT, WPARAM, LPARAM);
+public:
+	App(HINSTANCE);
+	int run(int);
+};
